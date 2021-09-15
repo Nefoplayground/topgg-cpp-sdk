edeb308833893a2d3283a17c73ff3ea7ea82cca8
Nefomemes <nefothingy@hotmail.com>
2021-09-15 17:19:47 +0700

    fix: class "dbl::Api" has no member "search_bot"

--- include/api.h
+++ include/api.h
@@ -15,6 +15,7 @@ namespace dbl {
             Api(const string & tkn);
             dbl::User get_user(const string &);
             dbl::Bot get_bot(const string &);
+            BotSearch Api::search_bot(const string & query)
             std::vector<bot_vote_t> get_last_1000_votes(const string botId);
             bool has_user_voted(const string botId, const string userId);
 
